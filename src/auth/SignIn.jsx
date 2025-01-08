287258061c6cb5abd382a848ccec94a766ab2e6f
Mahid970 <mmahidujjaman@gmail.com>
2025-01-08 18:24:43 +0600

    completed desgining the sign in page

--- src/auth/SignIn.jsx
+++ src/auth/SignIn.jsx
@@ -1,7 +1,74 @@
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
+import { BsGithub } from "react-icons/bs";
+import { Link } from "react-router";
 const SignIn = () => {
   return (
-    <div>
-      <h2>This is Sign in page</h2>
+    <div className="bg-base-200 min-h-screen ">
+      <div className="w-1/2 flex mx-auto py-24 text-xl font-semibold ">
+        <div className=" p-12  bg-base-100 w-full  shrink-0 shadow-2xl">
+          <form className=" flex flex-col gap-4">
+            <div className="">
+              <p className="mb-2">Email</p>
+
+              <input
+                type="email"
+                placeholder="Type your email"
+                className=" border-2 font-normal w-full p-2"
+                required
+              />
+            </div>
+            <div className="">
+              <div className="">
+                <p className="mb-2">Password</p>
+
+                <input
+                  type="password"
+                  placeholder="Type your password"
+                  className=" border-2 font-normal  w-full p-2"
+                  required
+                />
+              </div>
+              <label className="">
+                <a href="#" className=" text-sm font-normal">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="">
+              <button className="w-full bg-blue-500 py-2 font-bold text-white ">
+                Sign In
+              </button>
+            </div>
+          </form>
+          <div>
+            <div className="divider">Or use one of these options</div>
+            <div className="flex text-4xl ">
+              <div className="mx-auto flex gap-8">
+                <button className="border hover:border-blue-500 p-3">
+                  <FaSquareXTwitter />
+                </button>
+                <button className="border hover:border-blue-500 p-3">
+                  {" "}
+                  <FcGoogle />
+                </button>
+                <button className="border hover:border-blue-500 p-3">
+                  <BsGithub />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="mt-4">
+            <h3 className="text-center text-sm">
+              Do not have an account? Go to{" "}
+              <Link className="text-blue-500" to="/signUp">
+                Sign Up
+              </Link>{" "}
+              Page{" "}
+            </h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/header/Navbar.jsx
+++ src/header/Navbar.jsx
@@ -2,20 +2,41 @@ import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between mx-24 my-3 ">
+    <div className="flex items-center justify-between mx-24 py-3">
       <div className="">
         <h3 className="  text-blue-500 font-bold text-3xl">Simple Auth</h3>
       </div>
       <div className="">
-        <ul className="flex gap-12 text-xl">
-          <NavLink to="/">
+        <ul className="flex gap-8 text-xl">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "border-2 px-4 py-1  border-blue-500 text-blue-500"
+                : " px-4 py-1"
+            }
+          >
             <li className="font-bold">Home</li>
           </NavLink>
 
-          <NavLink to="signIn">
+          <NavLink
+            to="signIn"
+            className={({ isActive }) =>
+              isActive
+                ? "border-2 px-4 py-1 border-blue-500 text-blue-500"
+                : " px-4 py-1"
+            }
+          >
             <li className="font-bold">Sign In</li>
           </NavLink>
-          <NavLink to="signUp">
+          <NavLink
+            to="signUp"
+            className={({ isActive }) =>
+              isActive
+                ? "border-2 px-4 py-1 border-blue-500 text-blue-500"
+                : " px-4 py-1"
+            }
+          >
             {" "}
             <li className="font-bold">Sign Up</li>
           </NavLink>
